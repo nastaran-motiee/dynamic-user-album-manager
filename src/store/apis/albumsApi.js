78539f777f6fd2014f851afa8412517f9b8fef3c
nnastaran83 <nnastaran83@gmail.com>
2023-09-07 05:02:42 +0000

    lesson 400 done

--- src/store/apis/albumsApi.js
+++ src/store/apis/albumsApi.js
@@ -1,11 +1,27 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { faker } from "@faker-js/faker";
 
+//TODO: This function is for development purpose only.. delete this function at the end of development.
+const pause = async (duration) => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, duration);
+    });
+};
+
 const albumsApi = createApi({
     reducersPath: "albums",
-    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3005" }),
+    baseQuery: fetchBaseQuery({
+        baseUrl: "http://localhost:3005",
+        fetchFn: async (...args) => {
+            await pause(1000);
+            return fetch(...args);
+        }
+    }),
+
     endpoints: (builder) => ({
+
         addAlbum: builder.mutation({
+            invalidatesTags: (result, error, user) => [{ type: 'Album', id: user.id }],
             query: (user) => {
                 return {
                     url: "/albums",
@@ -18,6 +34,8 @@ const albumsApi = createApi({
             }
         }),
         fetchAlbums: builder.query({
+
+            providesTags: (result, error, user) => [{ type: 'Album', id: user.id }],
             query: (user) => {
                 return {
                     url: "/albums",
