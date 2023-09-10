c587269ca7fcd4dc00ac7c974f6850ebf4bd248e
nnastaran83 <nnastaran83@gmail.com>
2023-09-10 14:43:56 +0000

    section completed

--- src/store/apis/photosApi.js
+++ src/store/apis/photosApi.js
@@ -1,46 +1,56 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { faker } from "@faker-js/faker";
 
-
 const photosApi = createApi({
-    reducerPath: 'photos',
-    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3005" }),
-    endpoints: (builder) => ({
+  reducerPath: "photos",
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3005" }),
+  endpoints: (builder) => ({
+    fetchPhotos: builder.query({
+      providesTags: (result, error, album) => {
+        const tags = result.map((photo) => {
+          return { type: "Photo", id: photo.id };
+        });
+        tags.push({ type: "AlbumPhoto", id: album.id });
+        return tags;
+      },
+      query: (album) => ({
+        url: "/photos",
+        method: "GET",
+        params: {
+          albumId: album.id,
+        },
+      }),
+    }),
 
-        fetchPhotos: builder.query({
-            query: (album) => ({
-                url: '/photos',
-                method: 'GET',
-                params: {
-                    albumId: album.id
-                }
+    addPhoto: builder.mutation({
+      invalidatesTags: (result, error, album) => {
+        return [{ type: "AlbumPhoto", id: album.id }];
+      },
+      query: (album) => ({
+        url: "/photos",
+        method: "POST",
+        body: {
+          albumId: album.id,
+          url: faker.image.url(150, 150, true),
+        },
+      }),
+    }),
 
+    removePhoto: builder.mutation({
+      invalidatesTags: (result, error, photo) => {
+        return [{ type: "Photo", id: photo.id }];
+      },
+      query: (photo) => ({
+        url: `/photos/${photo.id}`,
+        method: "DELETE",
+      }),
+    }),
+  }),
+});
 
-            }),
-        }),
-
-        addPhoto: builder.mutation({
-            query: (album) => ({
-                url: '/photos',
-                method: 'POST',
-                body: {
-                    albumId: album.id,
-                    url: faker.image.url(150, 150, true)
-                }
-            })
-        }),
-
-        removePhoto: builder.mutation({
-            query: (photo) => (
-                {
-                    url: `/albums/${photo.id}`,
-                    method: 'DELETE'
-                }
-            )
-        })
-    })
-}
-);
-
-export const { useFetchPhotosQuery, useAddPhotoMutation, useRemovePhotoMutation } = photosApi;
+export const {
+  useFetchPhotosQuery,
+  useAddPhotoMutation,
+  useRemovePhotoMutation,
+} = photosApi;
 export { photosApi };
